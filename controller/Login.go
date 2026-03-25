8acd69bd88204fe64bcce620454a5816534752a3
Daffa Ulhaq Purnama Lovind <daffaulhaq05@gmail.com>
2026-03-25 09:24:21 +0700

    Add LoginHandler for user authentication
    Implement LoginHandler to manage user login and cookies.

--- controller/Login.go
+++ controller/Login.go
@@ -1,1 +1,34 @@
 package controller
+
+import (
+	"363project/controller/service"
+	"fmt"
+	"net/http"
+)
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	// Ambil ID dri middleware
+	userIdCtx := r.Context().Value("id")
+
+	if userIdCtx == "" || userIdCtx == nil {
+		user, err := service.CreateAnonymousUser()
+		if err != nil {
+			http.Error(w, "Gagal registrasi user baru", http.StatusInternalServerError)
+			return
+		}
+
+		// Set cookie ke browser
+		cookie := &http.Cookie{
+			Name:     "id",
+			Value:    fmt.Sprint(user.Id),
+			Path:     "/",
+			HttpOnly: true,
+			MaxAge:   86400,
+		}
+		http.SetCookie(w, cookie)
+
+		fmt.Fprintf(w, "Login Berhasil! Selamat datang user baru (ID: %d)", user.Id)
+	} else {
+		fmt.Fprintf(w, "Welcome Back! Anda login sebagai User ID: %v", userIdCtx)
+	}
+}
