545d0b4726f5d19023dab6721884b90ebcecb189
Daffa Ulhaq Purnama Lovind <daffaulhaq05@gmail.com>
2026-03-25 09:26:34 +0700

    Initialize environment and database on startup
    Add initialization for environment variables, database connection, and automatic table creation.

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"363project/controller"
+	"363project/initializer"
+	"363project/middleware"
 	"log"
 	"net/http"
 )
 
+func init() {
+	initializer.LoadEnvVar()   // Baca .env
+	initializer.ConnecttoDB()  // Konek MySQL
+	initializer.SyncDatabase() // Bikin tabel otomatis
+}
+
 func main() {
-	http.HandleFunc("/login")
+	// Pasang rute /login yang dilindungi AuthMiddleware
+	http.HandleFunc("/login", middleware.AuthMiddleware(controller.LoginHandler))
+
+	log.Println("Server 363-Project jalan di http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
